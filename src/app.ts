e10962cb1d1b5affe3350eaec6b14e9edffa3c51
Nabil Patel <nabilpatel4012@gmail.com>
2025-12-03 00:21:12 +0530

    fix cors issue

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,7 @@ app.use(
       process.env.FRONTEND_URL || "http://localhost:3001",
       "http://localhost:8080",
       "https://gomyturf.nexpictora.com",
+      "https://gomyturf.pages.dev"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
