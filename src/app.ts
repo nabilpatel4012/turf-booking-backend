aa7420439bdebb2276631473650f5c77701ebfa6
Nabil Patel <nabilpatel4012@gmail.com>
2026-01-08 14:32:53 +0530

    update the ratelimit for APIs

--- src/app.ts
+++ src/app.ts
@@ -26,8 +26,8 @@ app.set("trust proxy", 1); // Trust first proxy (e.g. Nginx/Cloudflare)
 
 // Rate Limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 120, // Limit each IP to 120 requests per `window` (here, per 1 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
